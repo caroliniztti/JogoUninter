bdf9dcd8b6d61ec965ce2c7a53c49c234435f930
carol <caroliniconzatti3@gmail.com>
2025-03-23 22:13:42 -0300

    Creating asset folder Inserting all assets Creating the menu: background, sound and fonts Creating the const file

--- code/Const.py
+++ code/Const.py
@@ -1,4 +1,17 @@
 from pygame.examples.grid import WINDOW_WIDTH
+# C
+COLOR_BLUE = (0, 0 ,255)
+COLOR_WHITE = (255, 255, 255)
 
+# M
+MENU_OPTION = ('NOVO JOGO 1P',
+               'NOVO JOGO 2P - COOPERATIVO',
+               'NOVO JOGO 2P - COMPETITIVO',
+               'SCORE',
+               'SAIR')
+
+
+
+# W
 WIN_WIDTH = 600
 WIN_HEIGHT = 338
--- code/menu.py
+++ code/menu.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 import pygame.image
+from pygame import Surface, Rect
+from pygame.font import Font
+
+from code.Const import WIN_WIDTH, MENU_OPTION, COLOR_WHITE, COLOR_BLUE
 
 
 class Menu:
@@ -10,6 +14,31 @@ class Menu:
         self. rect = self.surf.get_rect(left=0, top=0)
 
     def run(self, ):
-        self.window.blit(source=self.surf, dest=self.rect)
-        pygame.display.flip()
-        pass
+
+        pygame.mixer_music.load('./asset/somMenu.mp3')
+        pygame.mixer_music.play(-1)
+        while True:
+            self.window.blit(source=self.surf, dest=self.rect)
+            self.menu_text(text_size=50, text= "GUERRA NAS NUVENS", text_color= COLOR_BLUE, text_center_pos=((WIN_WIDTH / 2), 70))
+
+            for i in range(len(MENU_OPTION)):
+                self.menu_text(text_size=45, text=MENU_OPTION[i], text_color=COLOR_WHITE,
+                               text_center_pos=((WIN_WIDTH / 2), 170 + 30 * i))
+
+
+
+
+            pygame.display.flip()
+
+            # Check for all events
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()  # Close Window
+                    quit()
+
+    def menu_text(self, text_size: int, text: str, text_color: tuple, text_center_pos: tuple):
+        text_font: Font = pygame.font.SysFont(name="Lucida Sans Typewriter", size=text_size)
+        text_surf: Surface = text_font.render(text, True, text_color).convert_alpha()
+        text_rect: (
+            Rect) = text_surf.get_rect(center=text_center_pos)
+        self.window.blit(source=text_surf, dest=text_rect)
